fc752053230955544a9484b2a21bacb6dcaa6aee
hemlatachelani <hemlatachelani@gmail.com>
2026-01-02 11:43:48 +0500

    arithmetic operations

---
+++ ArithmeticOperation.java
@@ -0,0 +1,23 @@
+/*
+Write a Java program to print the results of the following operations.  
+Test Data:
+a. -5 + 8 * 6
+b. (55+9) % 9
+c. 20 + -3*5 / 8
+d. 5 + 15 / 3 * 2 - 8 % 3 */
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        int a = -5+8*6;
+        int b = (55+9) % 9;
+        int c = 20 + -3*5 / 8;
+        int d =  5 + 15 / 3 * 2 - 8 % 3; 
+        System.out.println("The value of a is: "+a);
+         System.out.println("The value of b is: "+b);
+        System.out.println("The value of c is: "+c);
+         System.out.println("The value of d is: "+d);
+
+        
+    }
+
+}
