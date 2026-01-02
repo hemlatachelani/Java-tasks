fb2db7dba68a234cbd2fbbba96b498f5d65f5ac2
hemlatachelani <hemlatachelani@gmail.com>
2026-01-02 12:01:26 +0500

    Basic arithmetic operation

---
+++ BasicArithmeticOperation.java
@@ -0,0 +1,44 @@
+/*Basic Arithmetic Operations
+
+Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+Test Data:
+Input first number: 125
+Input second number: 24
+Expected Output :
+125 + 24 = 149
+125 - 24 = 101
+125 x 24 = 3000
+125 / 24 = 5
+125 mod 24 = 5 */
+
+import java.util.Scanner;
+
+public class BasicArithmeticOperation {
+    public static void main(String[] args) {
+    
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.print("enter first number:");
+        int num1 = sc.nextInt();
+        System.out.print("enter second number: ");
+        
+        int num2 = sc.nextInt();
+        
+    
+        
+        System.out.println(num1+" + "+num2+" ="+(num1+num2));
+        System.out.println(num1+" - "+num2+" ="+(num1-num2));
+         System.out.println(num1+" * "+num2+" ="+(num1*num2));
+        System.out.println(num1+" / "+num2+" ="+(num1/num2));
+         System.out.println(num1+" % "+num2+" ="+(num1%num2));
+
+
+
+
+
+
+
+
+    }
+    
+}
